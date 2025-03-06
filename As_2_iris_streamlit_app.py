348d72723f80a7737cbb62f96e3c9d1fd9d01d0b
BhoneKhantTheim99 <117364016+BhoneKhantTheim99@users.noreply.github.com>
2025-03-06 21:57:55 +0700

    Add files via upload

---
+++ As_2_iris_streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+# Streamlit UI
+st.title("🪴I own an Iris flowers Shop🌱")
+st.subheader("Enter the parameters for Iris flower.")
+sepal_length = st.number_input("Sepal Length(cm)", min_value=4.3, max_value= 7.9, value= 5.84)
+sepal_width = st.number_input("Sepal Width(cm)", min_value = 2.0, max_value = 4.4, value = 3.057)
+petal_length = st.number_input("Petal Length(cm)", min_value = 1.0, max_value = 6.9)
+petal_width = st.number_input("Petal Width(cm)", min_value = 0.1, max_value = 2.5)
+
+with open('knn_iris_model_scaled.pkl', 'rb') as f:
+    loaded_scaler = pickle.load(f)
+
+if st.button("Predict"):
+    input_features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    input_features = scaler.fit_transform(input_features)
+
+    value = loaded_scaler.predict(input_features)
+    print(value)
