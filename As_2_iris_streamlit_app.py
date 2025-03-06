9e89e947886a6d732c38e76cca0ed5b4d8dbbbc4
BhoneKhantTheim99 <117364016+BhoneKhantTheim99@users.noreply.github.com>
2025-03-06 22:31:12 +0700

    Update As_2_iris_streamlit_app.py

--- As_2_iris_streamlit_app.py
+++ As_2_iris_streamlit_app.py
@@ -10,6 +10,9 @@ sepal_width = st.number_input("Sepal Width(cm)", min_value = 2.0, max_value = 4.
 petal_length = st.number_input("Petal Length(cm)", min_value = 1.0, max_value = 6.9)
 petal_width = st.number_input("Petal Width(cm)", min_value = 0.1, max_value = 2.5)
 
+with open('knn_iris_model.pkl', 'rb') as f:
+    loaded_model = pickle.load(f)
+    
 with open('knn_iris_model_scaled.pkl', 'rb') as f:
     loaded_scaler = pickle.load(f)
 
@@ -17,5 +20,5 @@ if st.button("Predict"):
     input_features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     input_features = loaded_scaler.transform(input_features)
 
-    value = loaded_scaler.predict(input_features)
+    value = loaded_model.predict(input_features)
     print(value)
